d777b222c61a7d227242f2a138e6dfcee9281008
Asus-PC\Rian Andrivani <rianandrivani@gmail.com>
2017-03-23 08:59:37 +0700

    Edit tSalesProductQuantityDetailDA

--- salesForceLibrary/src/main/java/library/salesforce/dal/tSalesProductQuantityDetailDA.java
+++ salesForceLibrary/src/main/java/library/salesforce/dal/tSalesProductQuantityDetailDA.java
@@ -21,11 +21,17 @@ public class tSalesProductQuantityDetailDA {
         tSalesProductQuantityDetailData dt = new tSalesProductQuantityDetailData();
         String CREATE_CONTACTS_TABLE = "CREATE TABLE IF NOT EXISTS " + TABLE_CONTACTS + "("
                 + dt.Property_intId + " TEXT NULL,"
+                + dt.Property_dtDate + " TEXT NULL,"
+                + dt.Property_intPrice + " TEXT NULL,"
+                + dt.Property_txtCodeProduct + " TEXT NULL,"
+                + dt.Property_txtKeterangan + " TEXT NULL,"
                 + dt.Property_txtProduct + " TEXT NULL,"
                 + dt.Property_txtExpireDate + " TEXT NULL,"
                 + dt.Property_txtQuantity + " TEXT NULL,"
+                + dt.Property_intTotal + " TEXT NULL,"
                 + dt.Property_txtNoSo + " TEXT NULL,"
-                + dt.Property_intActive + " TEXT NULL" + ")";
+                + dt.Property_intActive + " TEXT NULL,"
+                + dt.Property_txtNIK + " TEXT NULL" + ")";
         db.execSQL(CREATE_CONTACTS_TABLE);
     }
 
@@ -44,17 +50,30 @@ public class tSalesProductQuantityDetailDA {
     public void SaveDatatSalesProductQuantityDetailData(SQLiteDatabase db, tSalesProductQuantityDetailData data) {
         tSalesProductQuantityDetailData dt = new tSalesProductQuantityDetailData();
         db.execSQL("INSERT OR REPLACE into " +TABLE_CONTACTS+ " (" +dt.Property_intId+ ","
+                +dt.Property_dtDate+","
+                +dt.Property_intPrice+","
+                +dt.Property_txtCodeProduct+","
+                +dt.Property_txtKeterangan+","
                 +dt.Property_txtProduct+","
                 +dt.Property_txtExpireDate+","
                 +dt.Property_txtQuantity+","
-                + dt.Property_txtQuantity + ","
-                + dt.Property_txtNoSo + ") "+
+                +dt.Property_intTotal+","
+                +dt.Property_intTotal+","
+                +dt.Property_txtNoSo+","
+                +dt.Property_intActive+","
+                +dt.Property_txtNIK+") "+
             "values('" +String.valueOf(data.getIntId())+"','"
+                +String.valueOf(data.get_dtDate())+"','"
+                +String.valueOf(data.get_intPrice())+"','"
+                +String.valueOf(data.get_txtCodeProduct())+"','"
+                +String.valueOf(data.get_txtKeterangan())+"','"
                 +String.valueOf(data.getTxtProduct())+"','"
                 +String.valueOf(data.getTxtExpireDate())+"','"
                 +String.valueOf(data.getTxtQuantity())+"','"
+                +String.valueOf(data.get_intTotal())+"','"
                 +String.valueOf(data.get_txtNoSo())+"','"
-                +String.valueOf(data.get_intActive())+"')");
+                +String.valueOf(data.get_intActive())+"','"
+                +String.valueOf(data.get_txtNIK())+"')");
     }
 
     // Adding new contact
@@ -68,14 +87,16 @@ public class tSalesProductQuantityDetailDA {
     public tSalesProductQuantityDetailData getData(SQLiteDatabase db, int id) {
         tSalesProductQuantityDetailData dt = new tSalesProductQuantityDetailData();
         Cursor cursor = db.query(TABLE_CONTACTS, new String[] {dt.Property_intId,
-                dt.Property_txtProduct, dt.Property_txtExpireDate, dt.Property_txtQuantity
-                ,dt.Property_txtNoSo,dt.Property_intActive}, dt.Property_intId + "=?",
+                dt.Property_dtDate,dt.Property_intPrice,dt.Property_txtCodeProduct,dt.Property_txtKeterangan,
+                dt.Property_txtProduct, dt.Property_txtExpireDate, dt.Property_txtQuantity, dt.Property_intTotal
+                ,dt.Property_txtNoSo,dt.Property_intActive,dt.Property_txtNIK}, dt.Property_intId + "=?",
                 new String[] { String.valueOf(id) }, null, null, null, null);
         if (cursor != null)
             cursor.moveToFirst();
         tSalesProductQuantityDetailData contact = new tSalesProductQuantityDetailData(String.valueOf(cursor.getString(0)),
-                cursor.getString(1), cursor.getString(2), cursor.getString(3),
-                cursor.getString(4), cursor.getString(5));
+                cursor.getString(1), cursor.getString(2),cursor.getString(3), cursor.getString(4), cursor.getString(5),
+                cursor.getString(6), cursor.getString(7), cursor.getString(8), cursor.getString(9), cursor.getString(10),
+                cursor.getString(11));
         cursor.close();
         return contact;
     }
@@ -85,8 +106,9 @@ public class tSalesProductQuantityDetailDA {
         List<tSalesProductQuantityDetailData> contactList = null;
         tSalesProductQuantityDetailData dt = new tSalesProductQuantityDetailData();
         Cursor cursor = db.query(TABLE_CONTACTS, new String[] { dt.Property_intId,
-                dt.Property_txtProduct, dt.Property_txtExpireDate, dt.Property_txtQuantity
-                , dt.Property_txtNoSo, dt.Property_intActive}, dt.Property_txtNoSo +"=?",
+                dt.Property_dtDate,dt.Property_intPrice,dt.Property_txtCodeProduct,dt.Property_txtKeterangan,dt.Property_txtProduct,
+                dt.Property_txtExpireDate, dt.Property_txtQuantity,dt.Property_txtNIK,dt.Property_intTotal,
+                dt.Property_txtNoSo, dt.Property_intActive}, dt.Property_txtNoSo +"=?",
                 new String[] { String.valueOf(NoSo) },null,null,null,null);
         if (cursor != null){
             if (cursor.moveToFirst()) {
@@ -94,11 +116,17 @@ public class tSalesProductQuantityDetailDA {
                 do {
                     tSalesProductQuantityDetailData contact = new tSalesProductQuantityDetailData();
                     contact.setIntId(String.valueOf(cursor.getString(0)));
-                    contact.setTxtProduct(cursor.getString(1));
-                    contact.setTxtExpireDate(cursor.getString(2));
-                    contact.setTxtQuantity(cursor.getString(3));
-                    contact.set_txtNoSo(cursor.getString(4));
-                    contact.set_intActive(cursor.getString(5));
+                    contact.set_dtDate(cursor.getString(1));
+                    contact.set_intPrice(cursor.getString(2));
+                    contact.set_txtCodeProduct(cursor.getString(3));
+                    contact.set_txtKeterangan(cursor.getString(4));
+                    contact.setTxtProduct(cursor.getString(5));
+                    contact.setTxtExpireDate(cursor.getString(6));
+                    contact.setTxtQuantity(cursor.getString(7));
+                    contact.set_txtNIK(cursor.getString(8));
+                    contact.set_intTotal(cursor.getString(9));
+                    contact.set_txtNoSo(cursor.getString(10));
+                    contact.set_intActive(cursor.getString(11));
                     contactList.add(contact);
                 } while (cursor.moveToNext());
             }
@@ -121,11 +149,17 @@ public class tSalesProductQuantityDetailDA {
             do {
                 tSalesProductQuantityDetailData contact = new tSalesProductQuantityDetailData();
                 contact.setIntId(String.valueOf(cursor.getString(0)));
-                contact.setTxtProduct(cursor.getString(1));
-                contact.setTxtExpireDate(cursor.getString(2));
-                contact.setTxtQuantity(cursor.getString(3));
-                contact.set_txtNoSo(cursor.getString(4));
-                contact.set_intActive(cursor.getString(5));
+                contact.set_dtDate(cursor.getString(1));
+                contact.set_intPrice(cursor.getString(2));
+                contact.set_txtCodeProduct(cursor.getString(3));
+                contact.set_txtKeterangan(cursor.getString(4));
+                contact.setTxtProduct(cursor.getString(5));
+                contact.setTxtExpireDate(cursor.getString(6));
+                contact.setTxtQuantity(cursor.getString(7));
+                contact.set_txtNIK(cursor.getString(8));
+                contact.set_intTotal(cursor.getString(9));
+                contact.set_txtNoSo(cursor.getString(10));
+                contact.set_intActive(cursor.getString(11));
                 contactList.add(contact);
             } while (cursor.moveToNext());
         }
@@ -147,11 +181,17 @@ public class tSalesProductQuantityDetailDA {
             do {
                 tSalesProductQuantityDetailData contact = new tSalesProductQuantityDetailData();
                 contact.setIntId(String.valueOf(cursor.getString(0)));
-                contact.setTxtProduct(cursor.getString(1));
-                contact.setTxtExpireDate(cursor.getString(2));
-                contact.setTxtQuantity(cursor.getString(3));
-                contact.set_txtNoSo(cursor.getString(4));
-                contact.set_intActive(cursor.getString(5));
+                contact.set_dtDate(cursor.getString(1));
+                contact.set_intPrice(cursor.getString(2));
+                contact.set_txtCodeProduct(cursor.getString(3));
+                contact.set_txtKeterangan(cursor.getString(4));
+                contact.setTxtProduct(cursor.getString(5));
+                contact.setTxtExpireDate(cursor.getString(6));
+                contact.setTxtQuantity(cursor.getString(7));
+                contact.set_txtNIK(cursor.getString(8));
+                contact.set_intTotal(cursor.getString(9));
+                contact.set_txtNoSo(cursor.getString(10));
+                contact.set_intActive(cursor.getString(11));
                 contactList.add(contact);
             } while (cursor.moveToNext());
         }
@@ -166,13 +206,13 @@ public class tSalesProductQuantityDetailDA {
     // Deleting single contact
     public void deleteContact(SQLiteDatabase db, String id) {
         tSalesProductQuantityDetailData dt = new tSalesProductQuantityDetailData();
-        db.delete(TABLE_CONTACTS, dt.Property_intId + " =? ",
+        db.delete(TABLE_CONTACTS, dt.Property_intId + " = ? ",
                 new String[] {String.valueOf(id)});
     }
     public void deleteByNOSO(SQLiteDatabase db, String NOSO) {
         tSalesProductQuantityDetailData dt = new tSalesProductQuantityDetailData();
         db.delete(TABLE_CONTACTS, dt.Property_txtNoSo + " = ?",
-                new String[] {NOSO});
+                new String[] { NOSO });
     }
 
     // Getting contacts Count
@@ -186,15 +226,46 @@ public class tSalesProductQuantityDetailDA {
         return intres;
     }
 
-    /*// push data
+    // push data
     public List<tSalesProductQuantityDetailData> getAllDataToPushData(SQLiteDatabase db, List<tSalesProductQuantityDetailData> ListOfSalesProductQuantityHeader) {
         List<tSalesProductQuantityDetailData> contactList = null;
         // select All Query
         tSalesProductQuantityDetailData dt = new tSalesProductQuantityDetailData();
-        String tSalesProductQuantutyHeader = "()";
+
+        String tSalesProductQuantityHeader = "()";
 
         if (ListOfSalesProductQuantityHeader != null){
-            tSalesProductQuantutyHeader = "(";
+            tSalesProductQuantityHeader = "(";
+            for (int i = 0; i < ListOfSalesProductQuantityHeader.size(); i++) {
+                tSalesProductQuantityHeader = tSalesProductQuantityHeader + "'" + ListOfSalesProductQuantityHeader.get(i).get_txtNoSo() + "'";
+                tSalesProductQuantityHeader = tSalesProductQuantityHeader + ((i + 1) != ListOfSalesProductQuantityHeader.size() ? "," : ")");
+            }
         }
-    }*/
+        String selectQuery = "SELECT "+dt.Property_All+" FROM " + TABLE_CONTACTS +" WHERE "+dt.Property_txtNoSo+" IN " + tSalesProductQuantityHeader;
+
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        // looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            contactList = new ArrayList<tSalesProductQuantityDetailData>();
+            do {
+                tSalesProductQuantityDetailData contact = new tSalesProductQuantityDetailData();
+                contact.setIntId(cursor.getString(0));
+                contact.set_dtDate(cursor.getString(1));
+                contact.set_intPrice(cursor.getString(2));
+                contact.set_txtCodeProduct(cursor.getString(3));
+                contact.set_txtKeterangan(cursor.getString(4));
+                contact.setTxtProduct(cursor.getString(5));
+                contact.setTxtExpireDate(cursor.getString(6));
+                contact.setTxtQuantity(cursor.getString(7));
+                contact.set_intTotal(cursor.getString(8));
+                contact.set_txtNoSo(cursor.getString(9));
+                contact.set_intActive(cursor.getString(10));
+                contact.set_txtNIK(cursor.getString(11));
+                contactList.add(contact);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        return contactList;
+    }
 }
